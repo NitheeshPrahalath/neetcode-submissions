bd819ad2ae7933f7af8b7f737d38dd41b84e7765
C.S.Nitheesh Prahalath <94346804+NitheeshPrahalath@users.noreply.github.com>
2026-06-11 10:05:50 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        bracket = dict()
+        i =0
+        while i < len(nums):
+            diff = target -nums[i]
+            if diff in bracket:
+                return [bracket[diff], i]
+            bracket[nums[i]] = i
+            i +=1
